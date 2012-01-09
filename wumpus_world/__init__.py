0fa3bddc9631f64b8f2513212fd1bd60f5752fb1
Krzysztof Urban <krzysztof.urban@netguru.pl>
2012-01-09 16:15:13 +0100

    template for simpack generated by GarlicSim

---
+++ wumpus_world/__init__.py
@@ -0,0 +1,12 @@
+'''
+wumpus_world description.
+'''
+# (The above docstring should be in ReStructuredText.)
+
+from .state import State
+
+# Human-readable name of your simpack:
+name = '''wumpus_world'''
+
+# List of tags like 'game-theory', 'electronics', 'climate', etc.
+tags = []
